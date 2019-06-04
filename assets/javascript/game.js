03fcf725cbbdec6e2e0005f60fadbb2c561a8ef4
Nico Barbarese <nicobarbarese@gmail.com>
2019-06-04 15:01:39 -0400

    game almost complete

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,44 +1,51 @@
-var letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var wins = 0;
 var losses = 0;
 var guessesLeft = 9;
 var guessed = [];
+var computerGuess = letters[Math.floor(Math.random() * letters.length)];
+console.log(computerGuess);
 
 var userGuesses = document.getElementById("guesses")
+var userWins = document.getElementById("win-count")
 var userLosses = document.getElementById("loss-count")
 var userGuessesLeft = document.getElementById("guesses-left")
 
-function updateWins() {
-    document.getElementById("win-count").innerHTML = wins;
-  }
-
-function restartGame () {
+function restartGame() {
+    computerGuess = letters[Math.floor(Math.random() * letters.length)];
+    guessesLeft = 9;
+    guessed = [];
 }
-var computerGuess = letters[Math.floor(Math.random() * letters.length)]
 
-document.onkeyup = function(event) {
+function display() {
+    userGuessesLeft.innerHTML = guessesLeft;
+    userWins.innerHTML = wins;
+    userLosses.innerHTML = losses;
+}
 
-var userGuess = event.key;
+display();
+document.onkeyup = function (event) {
 
-if (letters.indexOf(userGuess) > -1)
-    if ((userGuess === computerGuess) && (guessesLeft > 0)) {
-        wins++;
-        updateWins();
-        computerGuess = letters[Math.floor(Math.random() * letters.length)];
-        
-    } else if ((userGuess !== computerGuess) && (guessesLeft > 0)) {
-        guessesLeft--;
-        userGuessesLeft.innerHTML = guessesLeft;
-        guessed.push(userGuess);
-        console.log(guessed);
-    }
-    else {
-        losses++;
-        userLosses.innerHTML = losses
-        guessesLeft = 9;
-        guessed = [];
-        computerGuess = letters[Math.floor(Math.random() * letters.length)]
-    }
+    var userGuess = event.key;
+
+    if (letters.indexOf(userGuess) > -1)
+        if ((userGuess === computerGuess) && (guessesLeft > 0)) {
+            wins++;
+            userWins.innerHTML = wins;
+            restartGame();
+
+
+        } else if ((userGuess !== computerGuess) && (guessesLeft > 0)) {
+            guessesLeft--;
+            userGuessesLeft.innerHTML = guessesLeft;
+            guessed.push(userGuess);
+            userGuesses.innerHTML = guessed;
+        }
+        else {
+            losses++;
+            userLosses.innerHTML = losses;
+            restartGame();
+        }
 }
 
 
